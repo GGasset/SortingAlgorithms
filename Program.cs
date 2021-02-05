1427e72177e741bce7246ae6ec59529e3bef2092
Germán <GassetGerman@gmail.com>
2021-02-05 16:46:21 +0100

    initial Commit with Selection Sort finished

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sorting_Algorithms
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> toSort = new List<int>(new int[] { 3, 1, 6, 9, 46, 91, 64, 79 });
+            List<int> sorted = SortingAlgorithms.SelectionSort(toSort);
+            foreach (int item in sorted)
+                Console.Write(item + " ");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ SortingAlgorithms.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sorting_Algorithms
+{
+    class SortingAlgorithms
+    {
+        public static List<int> SelectionSort(List<int> toSort)
+        {
+            int initialLength = toSort.Count;
+            List<int> output = new List<int>();
+            for (int i = 0; i < initialLength; i++)
+            {
+                int minValueIndex = GetIndexWithLowestValue(toSort);
+                output.Add(toSort[minValueIndex]);
+                toSort.RemoveAt(minValueIndex);
+            }
+            return output;
+        }
+
+        private static int GetIndexWithLowestValue(List<int> list)
+        {
+            int lowestValue = int.MaxValue, lowestIndex = -1;
+            for (int i = 0; i < list.Count; i++)
+                if (list[i] < lowestValue)
+                    lowestValue = list[lowestIndex = i];
+            return lowestIndex;
+        }
+    }
+}
